87b0a63c5a1a70fe4878f00e38c319e4a550194d
Taeho <k1mta3h0@gmail.com>
2023-11-22 23:45:01 +0900

    add hold function in printGame

--- tetris.c
+++ tetris.c
@@ -7,6 +7,14 @@
 
 typedef unsigned char u_char;
 
+void cursorView()
+{
+    CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+    cursorInfo.dwSize = 1;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
 void textColor(char colorNum) {
     int color = colorNum;
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
@@ -30,8 +38,15 @@ char Tetromino[7];
 
 u_char key[2] = {0,0};
 
-//up,down,left,right,drop,turnLeft,turnRight,hold,escape,select
-u_char keymap[10][2];
+//up,down,left,right,drop,turnClock,turnCounterClock,hold,escape,select
+u_char keymap[10][2] = {{224,72},{224,80},{224,75},{224,77},{224,80},{224,72},{122,0},{99,0},{27,0},{13,0}};
+
+char isKeyKeymap(char keymapNum) {
+    char ret = 0;
+    if (key[0] == keymap[keymapNum][0]&&key[1] == keymap[keymapNum][1])
+        ret = 1;
+    return ret;
+}
 
 //I,O,J,L,S,Z,T
 char Rotation[7][4][4][2] = {
@@ -59,17 +74,50 @@ char isTetrominoAtBottom(void);
 void printToGridDisplay(void);
 void printGame(char,char,char,char,char);
 void printDisplay(void);
+void printMainScreen(char);
+void printEscapeScreen(char);
+void updateKey(void);
+void resetKey(void);
+void updateKeymap(char [],char [],char [],char [],char [],char [],char [],char [],char [],char []);
+void mainGame(void);
 
 int main() {
     srand(time(NULL));
+    cursorView();
+    resetBlock();
+    resetTetromino();
+    resetGrid();
+    resetGridDisplay();
+    resetDisplay();
+    char select = 0;
+    printMainScreen(0);
+    printDisplay();
+    while (1) {
+        resetKey();
+        updateKey();
+        select += (isKeyKeymap(1)&&(select <= 4));
+        select -= (isKeyKeymap(0)&&(select >= 0));
+        select %= 4;
+        printMainScreen(select);
+        system("cls");
+        printDisplay();
+        Sleep(100);
+        if (select == 1&&isKeyKeymap(9)) {
+            mainGame();
+        }
+        if (select == 2&&isKeyKeymap(9)) {}
+        if (select == 3&&isKeyKeymap(9))
+            break;
+    }
 }
 
 void mainGame(void) {
+    resetDisplay();
     char score = 0;
-    char nextBlock = rand()%7;
-    char nextCol = rand()%7;
-    char holdBlock = 0;
-    char holdCol = 0;
+    char nextBlock = 0;
+    char nextCol = 0;
+    char holdBlock = 5;
+    char holdCol = 5;
     createTetromino(rand()%7,rand()%7,0);
     while (1) {
         nextBlock = rand()%7;
@@ -77,7 +125,9 @@ void mainGame(void) {
         fallTetromino();
         printToGridDisplay();
         printGame(nextBlock,nextCol,score,holdBlock,holdCol);
+        system("cls");
         printDisplay();
+        Sleep(100);
     }
 }
 
@@ -278,6 +328,12 @@ void printGame(char nextType,char nextCol,char score,char hold,char holdCol) {
         }
     }
     for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            Display[i+7][j+15][0] = ' ';
+            Display[i+7][j+15][1] = 15;
+        }
+    }
+    for (i = 0; i < 4; i++) {
         Display[-Rotation[nextType][0][i][1]+9][Rotation[nextType][0][i][0]+17][0] = '#';
         Display[-Rotation[nextType][0][i][1]+9][Rotation[nextType][0][i][0]+17][1] = BlockColor[nextCol];
     }
@@ -287,10 +343,17 @@ void printGame(char nextType,char nextCol,char score,char hold,char holdCol) {
     }
     for (i = 15; i < 21; i++) {
         for (j = 14; j < 20; j++) {
-            if ((i == 6)||(i == 11)||(j == 14)||(j == 19)) {
+            if ((i == 15)||(i == 20)||(j == 14)||(j == 19)) {
                 Display[i][j][0] = '%';
                 Display[i][j][1] = 15;
             }
+        }
+    }
+    //not made y coord not right
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            Display[i+7][j+15][0] = ' ';
+            Display[i+7][j+15][1] = 15;
         }
     }
     if (hold != 0) {
@@ -301,27 +364,92 @@ void printGame(char nextType,char nextCol,char score,char hold,char holdCol) {
     }
 }
 
-void updateKeymap(void) {
+void updateKeymap(char up[],char down[],char left[],char right[],char drop[],char turnClock[],char turnCounterClock[],char hold[],char escape[],char select[]) {
+    char i;
+    keymap[i][0] = up[0];
+    keymap[i][1] = up[1];
+    keymap[i][0] = down[0];
+    keymap[i][1] = down[1];
+    keymap[i][0] = left[0];
+    keymap[i][1] = left[1];
+    keymap[i][0] = right[0];
+    keymap[i][1] = right[1];
+    keymap[i][0] = drop[0];
+    keymap[i][1] = drop[1];
+    keymap[i][0] = turnClock[0];
+    keymap[i][1] = turnClock[1];
+    keymap[i][0] = turnCounterClock[0];
+    keymap[i][1] = turnCounterClock[1];
+    keymap[i][0] = hold[0];
+    keymap[i][1] = hold[1];
+    keymap[i][0] = escape[0];
+    keymap[i][1] = escape[1];
+    keymap[i][0] = select[0];
+    keymap[i][1] = select[1];
 }
 
 void updateKey(void) {
-    key[0] = 0;
-    key[1] = 0;
-    key[0] = getch();
-    if ((key[0] == 0)||(key[0] == 224))
-        key[1] = getch();
+    if (kbhit() != 0) {
+        key[0] = getch();
+        if ((key[0] == 0)||(key[0] == 224))
+            key[1] = getch();
+    }
 }
 
-void printMainscreen(void) {
+void resetKey(void) {
+    key[0] = 0;
+    key[1] = 0;
+}
+
+void printMainScreen(char select) {
+    char text1[6] = "Tetris";
+    char text2[6] = "[Play]";
+    char text3[6] = "[Load]";
+    char text4[6] = "[Quit]";
+    char i;
+    for (i = 0; i < 6; i++) {
+        Display[0][i][0] = text1[i];
+        Display[0][i][1] = BlockColor[i];
+        Display[2][i][0] = text2[i];
+        Display[2][i][1] = (select==1)*(-5)+15;
+        Display[3][i][0] = text3[i];
+        Display[3][i][1] = (select==2)*(-5)+15;
+        Display[4][i][0] = text4[i];
+        Display[4][i][1] = (select==3)*(-5)+15;
+    }
+}
+
+void printEscapeScreen(char select) {
+    char text1[4] = "Menu";
+    char text2[10] = "[Continue]";
+    char text3[6] = "[Load]";
+    char text4[8] = "[Option]";
+    char i;
+    for (i = 0; i < 4; i++) {
+        Display[0][i][0] = text1[i];
+        Display[0][i][1] = 15;
+    }
+    for (i = 0; i < 10; i++) {
+        Display[1][i][0] = text2[i];
+        Display[1][i][1] = (select==1)*(-5)+15;
+    }
+    for (i = 0; i < 6; i++) {
+        Display[2][i][0] = text3[i];
+        Display[2][i][1] = (select==2)*(-5)+15;
+    }
+    for (i = 0; i < 8; i++) {
+        Display[3][i][0] = text4[i];
+        Display[3][i][1] = (select==3)*(-5)+15;
+    }
 }
 
 void printDisplay(void) {
     char x;
     char y;
-    //cls
     for(y = 0; y < 24; y++) {
         for (x = 0; x < 21; x++) {
-            printf("%c",Display[y][x]);
+            textColor(Display[y][x][1]);
+            printf("%c",Display[y][x][0]);
         }
         printf("\n");
     }
